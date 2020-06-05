dcbcc7229d3bbf400e4362a5dece726421d5ccbc
xexa <arberi.fazlija@gmail.com>
2020-06-06 01:21:15 +0200

    Fixed Firestore error

--- app/src/main/java/com/example/loginexamplefirebase/Register.java
+++ app/src/main/java/com/example/loginexamplefirebase/Register.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,32 +14,48 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
-    private EditText fullName,emailEditText, passwordEditeText , phone ;
+
+    private static final String TAG = "Register";
+
+    private EditText fullNameEditeText,emailEditText, passwordEditeTex , phoneEditText ;
     private Button registerBtn;
     private TextView loginBtn;
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
+
+    private FirebaseFirestore db;
+
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        fullName = findViewById(R.id.fullName);
+        fullNameEditeText = findViewById(R.id.fullName);
         emailEditText = findViewById(R.id.Email);
-        passwordEditeText = findViewById(R.id.password);
-        phone = findViewById(R.id.phone);
+        passwordEditeTex = findViewById(R.id.password);
+        phoneEditText = findViewById(R.id.phone);
         registerBtn = findViewById(R.id.registerBtn);
         loginBtn = findViewById(R.id.createText);
 
         mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+
+        db = FirebaseFirestore.getInstance();
 
         if (mAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -48,8 +65,11 @@ public class Register extends AppCompatActivity {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String  email = emailEditText.getText().toString().trim();
-                String  password = passwordEditeText.getText().toString().trim();
+                final String  email = emailEditText.getText().toString().trim();
+                final String  password = passwordEditeTex.getText().toString().trim();
+
+                final String fullName = fullNameEditeText.getText().toString().trim();
+                final String phone = phoneEditText.getText().toString().trim();
 
                 if (email.isEmpty() && password.isEmpty()){
                     Toast.makeText(Register.this, "PLEASE ENTER EMAIL AND PASSWORD", Toast.LENGTH_SHORT).show();
@@ -57,17 +77,43 @@ public class Register extends AppCompatActivity {
                 }
 
                 if (password .length() < 6){
-                    passwordEditeText.setError("Password must be greater than 6 characters");
+                    passwordEditeTex.setError("Password must be greater than 6 characters");
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
 
                 mAuth.createUserWithEmailAndPassword(email,password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()){
                                     Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+
+                                    userId = mAuth.getCurrentUser().getUid();
+                                    DocumentReference documentReference = db.collection("users").document(userId);
+
+                                    Map<String , Object> user = new HashMap<>();
+                                    user.put("name" , fullName);
+                                    user.put("email", email);
+                                    user.put("phone", phone);
+
+                                    documentReference.set(user)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(Register.this, "Success", Toast.LENGTH_SHORT).show();
+                                                    Log.d(TAG, "user is created " + userId);
+
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(Register.this, "Failed", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
                                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                                 }else {
                                     Toast.makeText(Register.this, "Error: " + task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
