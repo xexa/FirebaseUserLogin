8349273bbcac135ae9d496b9c0ef438c562b5ad6
xexa <arberi.fazlija@gmail.com>
2020-06-06 15:55:16 +0200

    Fetch data from Firebase Firestore Database

--- app/src/main/java/com/example/loginexamplefirebase/MainActivity.java
+++ app/src/main/java/com/example/loginexamplefirebase/MainActivity.java
@@ -1,19 +1,61 @@
 package com.example.loginexamplefirebase;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView fullNameTextView;
+    private TextView emailTextView;
+    private TextView phoneTextView;
+
+    private ImageView imagePic;
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fullNameTextView = findViewById(R.id.full_name);
+        emailTextView = findViewById(R.id.email_text_view);
+        phoneTextView = findViewById(R.id.phone_text_view);
+
+        imagePic = findViewById(R.id.imageView);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        userId = mAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("users").document(userId);
+
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                assert documentSnapshot != null;
+                phoneTextView.setText(documentSnapshot.getString("phone"));
+                emailTextView.setText(documentSnapshot.getString("email"));
+                fullNameTextView.setText(documentSnapshot.getString("name"));
+            }
+        });
     }
 
     public void logout(View view) {
